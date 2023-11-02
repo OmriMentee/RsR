b389de894f7a576430b4cf932d8dc7bbada1f9d7
OmriMentee <omri@menteebot.com>
2023-11-02 20:17:43 +0200

    fixing parsing issue and start adding error counter

--- RsR.py
+++ RsR.py
@@ -6,12 +6,15 @@ class MyTool:
         self.data_dict = {}
         self.yml_dict = {}
         
+        
+        
         self.read_setup_data(self.motor_id)
         self.read_yml_data(self.yml_dict)
         
-        self.init_actual_data(self.motor_id)
+        # self.init_actual_data(self.motor_id)
         self.init_expected_data("ARM")
         
+        # self.update_motor_data(self.motor_id)
     
         
         
@@ -24,8 +27,35 @@ class MyTool:
         pass
         
     
+    # def read_setup_data(self, object_id):
+    # # Define the command with the object ID as a variable
+    #     command = f"mdtool setup info {object_id}"
+
+    #     try:
+    #         # Execute the command and capture the output
+    #         output = subprocess.check_output(command, shell=True, text=True)
+
+    #         # Split the output by lines and parse each line
+    #         for line in output.splitlines():
+    #             # Split each line by ":" to separate key and value
+    #             parts = line.strip().split(":")
+    #             if len(parts) == 2:
+    #                 key = parts[0].strip()
+    #                 key=key.lstrip("- ")
+    #                 value = parts[1].strip()
+    #                 self.data_dict[key] = value
+    #     except subprocess.CalledProcessError as e:
+    #         print(f"Error executing command: {e}")
+            
+    #     # for key, value in self.data_dict.items():
+    #     #     print(f"key:{key}")
+    #     #     print(f"val: {value}")
+    #     #     print("#################")
+        
+    #     #clean unnecessary chars after automated parser
+ 
     def read_setup_data(self, object_id):
-    # Define the command with the object ID as a variable
+        # Define the command with the object ID as a variable
         command = f"mdtool setup info {object_id}"
 
         try:
@@ -38,55 +68,68 @@ class MyTool:
                 parts = line.strip().split(":")
                 if len(parts) == 2:
                     key = parts[0].strip()
+                    key =key.lstrip("[MDTOOL]")
+                    key =key.lstrip("[CANDLE]")
+                    key =key.lstrip("- ")
                     value = parts[1].strip()
                     self.data_dict[key] = value
+
+            # Create class attributes from data_dict keys
+            for key, value in self.data_dict.items():
+                setattr(self, key, value)
+
         except subprocess.CalledProcessError as e:
             print(f"Error executing command: {e}")
-            
-        # for key, value in self.data_dict.items():
-        #     print(f"{key}: {value}")
-        
-        #clean unnecessary chars after automated parser
+ 
         
 
         
-    def init_actual_data(self, motor_id):
+    # def init_actual_data(self, motor_id):
         
-        # Initialize attributes with default values
-        self.actuator_name = self.data_dict['- actuator name'] #example for bad auto parsing. this should be 'actuator name' and its value should be
-        self.can_speed = ""
-        self.can_termination_resistor = ""
-        self.gear_ratio = ""
-        self.firmware_version = ""
-        self.hardware_version = ""
-        self.build_date = ""
-        self.commit_hash = ""
-        self.max_current = ""
-        self.bridge_type = ""
-        self.shunt_resistance = ""
-        self.pole_pairs = ""
-        self.kv_rating = ""
-        self.motor_shutdown_temperature = ""
-        self.motor_calibration_mode = ""
-        self.motor_torque_constant = ""
-        self.motor_stiction = ""
-        self.motor_friction = ""
-        self.d_axis_resistance = ""
-        self.d_axis_inductance = ""
-        self.torque_bandwidth = ""
-        self.can_watchdog = ""
-        self.output_encoder = ""
-        self.position = ""
-        self.velocity = ""
-        self.torque = ""
-        self.mosfet_temperature = ""
-        self.motor_temperature = ""
-        self.main_encoder_error = ""
-        self.calibration_error = ""
-        self.bridge_error = ""
-        self.hardware_error = ""
-        self.communication_error = ""
+    #     # Initialize attributes with default values
+    #     self.actuator_name = ""
+    #     self.can_speed = ""
+    #     self.can_termination_resistor = ""
+    #     self.gear_ratio = ""
+    #     self.firmware_version = ""
+    #     self.hardware_version = ""
+    #     self.build_date = ""
+    #     self.commit_hash = ""
+    #     self.max_current = ""
+    #     self.bridge_type = ""
+    #     self.shunt_resistance = ""
+    #     self.pole_pairs = ""
+    #     self.kv_rating = ""
+    #     self.motor_shutdown_temperature = ""
+    #     self.motor_calibration_mode = ""
+    #     self.motor_torque_constant = ""
+    #     self.motor_stiction = ""
+    #     self.motor_friction = ""
+    #     self.d_axis_resistance = ""
+    #     self.d_axis_inductance = ""
+    #     self.torque_bandwidth = ""
+    #     self.can_watchdog = ""
+    #     self.output_encoder = ""
+    #     self.position = ""
+    #     self.velocity = ""
+    #     self.torque = ""
+    #     self.mosfet_temperature = ""
+    #     self.motor_temperature = ""
+    #     self.main_encoder_error = ""
+    #     self.calibration_error = ""
+    #     self.bridge_error = ""
+    #     self.hardware_error = ""
+    #     self.communication_error = ""
         
+    def update_motor_data(self, motor_id):
+        # First, read the setup data into self.data_dict using read_setup_data
+        self.read_setup_data(motor_id)
+        
+        # Then, iterate through the attributes in init_actual_data
+        for attribute_name in dir(self):
+            if not attribute_name.startswith("__") and attribute_name in self.data_dict:
+                setattr(self, attribute_name, self.data_dict[attribute_name])
+
     
     def init_expected_data(self,robot_type): #type necessary?
         #this function should pars data from yamls:
@@ -115,9 +158,26 @@ class MyTool:
             print("")
         
         
-    def error_reporter(self,motor_id):
+    def error_reporter(self):
         #TODO: this function should extract errors from actual_data_dic and report them.
         # TBD: if any errors exist i would like to get notified abut it - let use our mail
+        cnt = 222
+        attributes_and_values = vars(self)
+        for attr, value in attributes_and_values.items():
+            if "error" in attr:
+                if "ALL OK" not in value:
+                    cnt=cnt+1
+                    print ("OMG ERROR")
+                else:
+                    print (value)
+        print(f"tot num of errors: {cnt}")
+        
+        # for attribute_name in dir(self):
+        #     if not attribute_name.startswith("__") and attribute_name in self.data_dict:
+        #         if "error" in attribute_name:
+        #             print (attribute_name)
+      
+        
         pass
     
     def encoder_tracker(self,motor_id):
@@ -139,10 +199,22 @@ def update_robo_state(self):
 
 def main():
     # Create an instance of the MyTool class
-    single_motor = MyTool(400,"ARM")
+    single_motor = MyTool(200,"ARM")
 
-    # Use the class's method
-    print(f"this is actuator's {single_motor.motor_id} name: {single_motor.get_actuator_name()}!")
+    # # Use the class's method
+    # print(f"this is actuator's {single_motor.motor_id} name: {single_motor.get_actuator_name()}!")
+
+    attributes_and_values = vars(single_motor)
+
+    # Print the attributes and their values
+    for attr, value in attributes_and_values.items():
+        if (attr!="data_dict"):
+            print(f"{attr}: {value}")
+            print("#### next ####")
+            
+    print("wdsda;lk")
+    
+    single_motor.error_reporter()
 
 if __name__ == "__main__":
     main()
